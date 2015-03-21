11f4bae71f90cb2f7a21f36f3c69c2e6a1c5a827
Marcus Povey <marcus@marcus-povey.co.uk>
2015-03-21 13:38:54 +0000

    First attempt

---
+++ known.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Known Shell API caller
+# Author: Marcus Povey <https://www.marcus-povey.co.uk>
+#
+# This is basically a wrapper around curl for posting things to the Known API from the shell
+#
+# Usage:
+# 	./known http://mysite.com/endpoint user api_key data
+#
+#####
+
+ACTION=$(echo -n $1 |cut -d'/' -f4-)
+HMAC=$(echo -n "${ACTION}$3" | sha256sum | cut -d' ' -f1 | base64 -w0)
+
+curl -i -H "Accept: application/json" -X POST \
+	-H "X-KNOWN-USERNAME: $2" \
+	-H "X-KNOWN-SIGNATURE: $HMAC" \
+	-d $4 $1
