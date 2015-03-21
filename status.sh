80a7e069c52f875c34cc62926d1ef134ad67585d
Marcus Povey <marcus@marcus-povey.co.uk>
2015-03-21 19:32:44 +0000

    Status post + syndication from the command line

---
+++ status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Known Shell Status message poster
+# Author: Marcus Povey <https://www.marcus-povey.co.uk>
+#
+# Wrapper around known.sh that posts a status message. Assumes known.sh is on your path.
+#
+# Usage:
+# 	echo "my status message" | ./status.sh http://mysite.com user api_key syndication....
+#
+#####
+
+read post
+
+data=$(echo -n $post | php -r "echo urlencode(file_get_contents('php://stdin'));")
+
+syndication="${@:4}"
+syn=""
+
+for word in $syndication;
+	do syn="${syn}syndication[]=$word&";
+done
+
+echo "${syn}body=$data" | ./known.sh "$1/status/edit" $2 $3
