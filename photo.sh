218872cb5837981b123e5cf47aafa986ea75fd4c
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-29 15:14:05 +0100

    Adding initial photo uploader

---
+++ photo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Known Shell photo message poster
+# Author: Marcus Povey <https://www.marcus-povey.co.uk>
+#
+# Wrapper around known.sh that posts a photo message. Assumes known.sh is on your path.
+#
+# Usage:
+# 	echo "body message" | ./photo.sh http://mysite.com user api_key title filename 
+#
+# At the moment syndication isn't supported, because I ran out of time working out the shell script.
+#
+#####
+
+read post
+
+data=$(echo -n "$post" | php -r "echo urlencode(file_get_contents('php://stdin'));")
+
+syndication="${@:6}"
+syn=""
+
+for word in $syndication;
+	do 
+	enc=$(echo -n $word | php -r  "echo urlencode(file_get_contents('php://stdin'));");
+	syn="${syn}syndication%5B%5D=$enc&";
+done
+
+#echo "${syn}title=${4}&body=${data}&photo=@${5};filename=alert.jpg;type=image/jpeg" | known.sh "$1/photo/edit" $2 $3
+
+
+ACTION='photo/edit'
+HMAC=$(echo -n "/$ACTION" | openssl dgst -binary -sha256 -hmac $3 |  base64 -w0)
+
+NEWDATA="${syn}title=${4}&body=${data}"
+
+# Temp cookie file
+COOKIE="/tmp/$(basename $0).$$.tmp"
+
+curl -sS -c $COOKIE -L \
+	-H "Accept: application/json" \
+        -H "X-KNOWN-USERNAME: $2" \
+        -H "X-KNOWN-SIGNATURE: $HMAC" \
+	-H "Content-Type: multipart/form-data"  \
+        -F "photo=@${5};filename=alert.jpg;type=image/jpeg" \
+        -F "title=${4}" \
+	-F "body=${data}" \
+	$1/photo/edit #| python -m json.tool
+
+rm $COOKIE
+
